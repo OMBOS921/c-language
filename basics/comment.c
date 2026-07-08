fd9382378f472dc9680437c13525f019295500e9
Om Gupta <omgupta210708@gmail.com>
2026-07-08 20:42:46 +0530

    Add files via upload

---
+++ basics/comment.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* program : first comment program
+by : om gupta
+*/
+
+int main(){
+    // print my name 
+    printf("my name is om gupta\n");
+    // print my gol
+    printf("NIT & MNC");
+    
+    return 0;
+}
